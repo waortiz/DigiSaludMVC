775dbcc70b47baed069ee05f69967806551f9682
William Ortiz <waortiz@gmail.com>
2019-10-01 13:47:37 -0500

    Inicio del Proyecto

---
+++ DigiSalud/Controllers/ClienteController.cs
@@ -0,0 +1,29 @@
+﻿using DigiSalud.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DigiSalud.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ingresar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingresar(Cliente cliente)
+        {
+            return View();
+        }
+    }
+}
