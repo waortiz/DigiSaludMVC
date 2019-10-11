57c3c914bd956a6f55105b018e8c84eec50f784a
William Ortiz <waortiz@gmail.com>
2019-10-11 14:37:35 -0500

    Ajustes

--- DigiSalud/Controllers/ClienteController.cs
+++ DigiSalud/Controllers/ClienteController.cs
@@ -12,12 +12,31 @@ namespace DigiSalud.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
+
             return View();
         }
 
         public ActionResult Ingresar()
         {
-            return View();
+            var tiposDocumento = new List<SelectListItem>();
+            tiposDocumento.Add(new SelectListItem()
+            {
+                Text = "Cédula de Ciudadanía",
+                Value = "1"
+            });
+            tiposDocumento.Add(new SelectListItem()
+            {
+                Text = "Tarjeta de Identidad",
+                Value = "2"
+            });
+            tiposDocumento.Add(new SelectListItem()
+            {
+                Text = "Tarjeta de Extranjería",
+                Value = "3"
+            });
+            ViewBag.TiposDocumento = tiposDocumento;
+
+            return View(new Cliente() { PrimerNombre="Pepito" });
         }
 
         [HttpPost]
--- DigiSalud/Models/Cliente.cs
+++ DigiSalud/Models/Cliente.cs
@@ -8,6 +8,14 @@ namespace DigiSalud.Models
     public class Cliente
     {
         public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public string NumeroDocumento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public bool Cotizante { get; set; }
         public List<int> Antecedentes { get; set; }
     }
 }
